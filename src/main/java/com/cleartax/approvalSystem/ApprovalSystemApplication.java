9381579d4f58ac03159ec76370461e0cc01af6da
Nitish Kushwaha <nitish.kushwaha@CTMCMHJ7D2.local>
2025-02-21 14:58:53 +0530

    consumer service working. added EnableScheduler in ApprovalSystemApplication.java

--- src/main/java/com/cleartax/approvalSystem/ApprovalSystemApplication.java
+++ src/main/java/com/cleartax/approvalSystem/ApprovalSystemApplication.java
@@ -3,8 +3,10 @@ package com.cleartax.approvalSystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApprovalSystemApplication {
 
 	public static void main(String[] args) {
